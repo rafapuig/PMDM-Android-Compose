ba9ce2622a85b986f2f5c9defdc5bc997bad9d4d
Rafa Puig <rafapuig.profe@gmail.com>
2025-11-13 22:14:08 +0100

    stringResource

---
+++ learningcompose/src/main/java/es/rafapuig/pmdm/compose/learning/resources/strings.kt
@@ -0,0 +1,29 @@
+package es.rafapuig.pmdm.compose.learning.resources
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewDynamicColors
+import es.rafapuig.pmdm.compose.learning.R
+
+@Composable
+fun StringResourcesDemo() {
+    Text(stringResource(R.string.hello_world))
+}
+
+@Preview(
+    showBackground = true)
+@Composable
+fun DefaultPreview() {
+    StringResourcesDemo()
+}
+
+@Preview(
+    showBackground = true,
+    locale = "es"
+    )
+@Composable
+fun SpanishPreview() {
+    StringResourcesDemo()
+}
